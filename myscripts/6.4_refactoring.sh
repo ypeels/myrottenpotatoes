66d9136866b9fba50cb4ab7d4cc513d6927c2773
ypeels <ypeels@live.com>
2012-10-26 11:05:54 -0700

    forgot to add 6.4 script

---
+++ myscripts/6.4_refactoring.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+pushd ~/myrottenpotatoes
+
+echo 
+echo "Second 'it' in spec/controllers/movies_controller_spec.rb"
+echo "# Section 6.4 Figure 6.6"
+echo http://pastebin.com/3HELV51D
+gedit spec/controllers/movies_controller_spec.rb
+
+echo
+echo "Refactor before :each in spec/controllers/movies_controller_spec.rb"
+echo "# Section 6.4 Figure 6.7"
+echo http://pastebin.com/dubDZGTQ
+gedit spec/controllers/movies_controller_spec.rb
+
+echo 
+echo Final it clause in spec/controllers/movies_controller_spec.rb
+echo "# Section 6.4 Figure 6.8"
+echo http://pastebin.com/ymzLR5ni
+gedit spec/controllers/movies_controller_spec.rb
+
+echo
+echo Final refactoring
+echo "# Section 6.4 Figure 6.9"
+echo http://pastebin.com/qg5nXEK0
+gedit spec/controllers/movies_controller_spec.rb
+
+
+popd
