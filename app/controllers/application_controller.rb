44a36c95bcf2fb426dcd8c28497449f48608db90
ypeels <ypeels@live.com>
2012-11-05 21:18:29 -0800

    Second erratum in Figure 7.7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil    
-    @current_user ||= Moviegoer.find_by_id(session[:user_id])
+    @current_user ||= Moviegoer.find_by_uid(session[:user_id])
     
     # login_path(.) argument - my extension of the 10/26/2012 14:15:44 erratum
     redirect_to login_path(:provider => 'twitter') and return unless @current_user
